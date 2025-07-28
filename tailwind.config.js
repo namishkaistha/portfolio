b061a34929dc5be0a9346a6f6208656f4ce64416
Namish Kaistha <namish.kaistha@ollion.com>
2025-07-28 16:09:15 -0500

    Add Tailwind safelist to preserve hardcoded colors for deployment

--- tailwind.config.js
+++ tailwind.config.js
@@ -5,6 +5,21 @@ module.exports = {
     "./src/app/**/*.{js,jsx,ts,tsx}",
     "./src/components/**/*.{js,jsx,ts,tsx}",
   ],
+  safelist: [
+    "bg-white",
+    "bg-gray-100",
+    "bg-gray-300",
+    "bg-gray-700",
+    "bg-gray-800",
+    "dark:bg-gray-700",
+    "dark:bg-gray-800",
+    "text-gray-200",
+    "dark:text-gray-200",
+    "border-gray-300",
+    "border-gray-600",
+    "dark:border-gray-600",
+    "dark:border-gray-700",
+  ],
   theme: {
     extend: {
       colors: {
