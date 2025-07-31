5cef59f9ce5c419bc38fba7f555a6e9064cf9be0
Namish Kaistha <namish.kaistha@ollion.com>
2025-07-30 22:10:00 -0500

    make podcard brightness changes

--- src/app/components/molecules/PodCard.jsx
+++ src/app/components/molecules/PodCard.jsx
@@ -29,7 +29,7 @@ export default function PodCard({ title, description, link }) {
         <div
           className="absolute inset-0 border-2 border-gray-600 dark:border-gray-600 rounded-lg flex items-center justify-center p-4 [backface-visibility:hidden]"
           style={{
-            backgroundColor: isDark ? "#374151" : "#f3f4f6",
+            backgroundColor: isDark ? "#374151" : "#FFFFFF",
             color: isDark ? "#f5f5dc" : "#2e5939",
           }}
         >
@@ -40,7 +40,7 @@ export default function PodCard({ title, description, link }) {
         <div
           className="absolute inset-0 border-2 border-gray-600 dark:border-gray-600 rounded-lg p-4 flex items-center justify-center [transform:rotateY(180deg)] [backface-visibility:hidden]"
           style={{
-            backgroundColor: isDark ? "#374151" : "#f3f4f6",
+            backgroundColor: isDark ? "#374151" : "#FFFFFF",
             color: isDark ? "#f5f5dc" : "#2e5939",
           }}
         >
